5e07d9a43daab7482bf9a28c4c12ca1eabd2a9ed
Patrick Wells <pwells@ucdavis.edu>
2022-11-09 10:49:09 -0800

    Add parse_types to importables

--- str2type/__init__.py
+++ str2type/__init__.py
@@ -1,5 +1,5 @@
-from .str2type import get_type, check_type
+from .str2type import get_type, check_type, parse_types
 
-__all__ = ["get_type", "check_type"]
+__all__ = ["get_type", "check_type", "parse_types"]
 
 __version__ = '0.1.0'
